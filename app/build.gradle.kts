3e7b06e6841856df0a1ae8d90613499d7fb65bd7
nenolinkdk <nenolinkdk@gmail.com>
2026-06-21 20:30:07 +0200

    Add debug APK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,4 +14,15 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+    }
 }
